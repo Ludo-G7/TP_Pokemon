f573c1ff72aa3d0045f3e4d83d71d51e7e0e4bd7
Ludo-G7 <ludovic.garel@hotmail.fr>
2023-03-17 17:35:41 +0100

    Mise à jour front

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PokedexRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,10 +16,66 @@ class MainController extends AbstractController
     }
 
 
-    #[Route('/pokedex', name:'main_pokedex')]
-    public function pokedex():Response
+    #[Route('/list', name:'main_pokedex')]
+    public function pokedex(PokedexRepository $pokedexRepository):Response
     {
-        return $this->render('main/pokedex.html.twig');
+        $pokelist = $pokedexRepository->findAll();
+        return $this->render('main/pokedex.html.twig',
+            [
+                'poke'=>$pokelist
+            ]);
     }
+
+    #[Route('/details/{id}', name:'main_details')]
+    public function details(int $id, PokedexRepository $pokedexRepository):Response
+    {
+        //Todo : chercher la série en fonction de son identifiant dans la BDD
+        $detailPok = $pokedexRepository->find($id);
+
+        return $this->render('main/details.html.twig',
+            [
+                //TODO : passer la série à twig
+                'detailPok' => $detailPok
+            ]);
+    }
+
+    #[Route('/recherche', name:'main_search_capture')]
+    public function rechercheByCapture(PokedexRepository $pokedexRepository):Response
+    {
+        $rechercheByCapture = $pokedexRepository->findBy(['is_captured'=>1], []);
+
+        return $this -> render('main/pokedex.html.twig',
+        [
+            'poke' => $rechercheByCapture
+        ]
+        );
+    }
+
+    #[Route('/recherche2', name:'main_search_nom')]
+    public function rechercheByNom(PokedexRepository $pokedexRepository):Response
+    {
+        $rechercheByNom = $pokedexRepository->findBy([], ['name'=>'ASC']);
+
+        return $this -> render('main/pokedex.html.twig',
+            [
+                'poke' => $rechercheByNom
+            ]
+        );
+    }
+
+    /*
+    #[Route('/capture', name:'main_capture')]
+    public function capture(PokedexRepository $pokedexRepository):Response
+    {
+        $capture = $pokedexRepository->;
+
+        return $this -> render('main/pokedex.html.twig',
+            [
+                'poke' => $rechercheByNom
+            ]
+        );
+    }
+*/
+
 
 }
---
+++ src/Entity/Pokedex.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PokedexRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PokedexRepository::class)]
+class Pokedex
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::TEXT,nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column]
+    private ?int $attaque = null;
+
+    #[ORM\Column]
+    private ?int $defense = null;
+
+    #[ORM\Column]
+    private ?bool $is_captured = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $image = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getAttaque(): ?int
+    {
+        return $this->attaque;
+    }
+
+    public function setAttaque(int $attaque): self
+    {
+        $this->attaque = $attaque;
+
+        return $this;
+    }
+
+    public function getDefense(): ?int
+    {
+        return $this->defense;
+    }
+
+    public function setDefense(int $defense): self
+    {
+        $this->defense = $defense;
+
+        return $this;
+    }
+
+    public function isis_captured(): ?bool
+    {
+        return $this->is_captured;
+    }
+
+    public function setis_captured(bool $is_captured): self
+    {
+        $this->is_captured = $is_captured;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+}
